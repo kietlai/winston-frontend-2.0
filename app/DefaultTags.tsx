f3219c6693b6bd4025328a9fe4e321648eaf95ec
Kiet Lai <64864561+kietlai@users.noreply.github.com>
2023-06-01 22:26:38 -0400

    Create DefaultTags.tsx

---
+++ app/DefaultTags.tsx
@@ -0,0 +1,9 @@
+export default function DefaultTags() {
+  return (
+    <>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link href="/Winston.svg" rel="icon"/>
+    </>
+  );
+}
